14c66cd2aeb3b116de30399ce9631988babeab76
psucharz <patryk.such25@gmail.com>
2023-05-10 14:46:46 +0200

    fixes to app due to interface changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { NoteComponent } from './note/note.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarComponent
+    CalendarComponent,
+    NoteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NoteInfo } from '../noteInfo.interface';
 
 @Component({
   selector: 'app-calendar',
@@ -8,6 +9,7 @@ import { Component } from '@angular/core';
 
 export class CalendarComponent {
   days: number[] = [];
+  hasNote: boolean[] = [];
   currentDate: Date = new Date();
   dayNames: string[] = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'];
 
@@ -19,9 +21,13 @@ export class CalendarComponent {
     const year: number = this.currentDate.getFullYear();
     const month: number = this.currentDate.getMonth();
     const numDays: number = new Date(year, month + 1, 0).getDate();
+    const notes: NoteInfo[] = JSON.parse(localStorage.getItem('notes') || '[]');
 
     for (let i = 1; i <= numDays; i++) {
+      const day: Date = new Date(year, month, i);
+      const hasNote: boolean = notes.some((note) => note.day == day.getDay());
       this.days.push(i);
+      this.hasNote.push(hasNote);
     }
   }
 }
